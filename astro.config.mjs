689a50a40f52a5730c468a6c0d2fde5fb882154a
Jesus Donate Sanchez <jesus.donate38@gmail.com>
2026-01-20 11:50:54 +0100

    Eliminar cadenas criticas e inlining

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://siempredivinas.com',
   integrations: [tailwind(), sitemap()],
-  
-  // ESTA ES LA PARTE QUE SOLUCIONA EL ERROR:
   build: {
-    inlineStylesheets: 'always' // Pega el CSS directamente en el HTML
-  }
+    inlineStylesheets: 'always' // Pega el CSS directamente para eliminar el archivo contacto.css de la red
   }
 });
